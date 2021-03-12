9c599b9ac169c4a364867f821bdc45bf3d800ccd
ditinex <asifakramsk@gmail.com>
2021-03-12 12:07:10 +0530

    boiler plate without unit test

--- __test__/index.spec.js
+++
@@ -1,9 +0,0 @@
-const Config = require('../config')
-
-global.config = {
-    apiurl: 'http://192.168.0.114:'+Config.port,
-    user: null
-}
-
-describe("\n\n--- Setting Test Environment. ---", require('./setup').Test)
-describe("\n\n--- Auth API tests. ---", require('./auth').Test)
